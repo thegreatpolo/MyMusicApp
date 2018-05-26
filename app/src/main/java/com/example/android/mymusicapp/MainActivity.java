a3f181e6ce83a816c5d4d57fa08e9804f4c256d2
[paulcristofari] <[paul.ivan.cristofari@gmail.com]>
2018-05-26 17:30:22 +0200

    Hides action bar and adds a NowPlaying activity, called on NowPlaying TextView

--- app/src/main/java/com/example/android/mymusicapp/MainActivity.java
+++ app/src/main/java/com/example/android/mymusicapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.mymusicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hideActionBar();
+
+
+
+        TextView nowplaying = (TextView) findViewById(R.id.now_playing);
+        nowplaying.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nowplayingIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+                startActivity(nowplayingIntent);
+            }
+        });
+
+    }
+
+    public void hideActionBar() {  // Hides the action bar
+        getSupportActionBar().hide();
     }
 }
